beae60472ce83ea1a5c8783e7dc7f6e2d5e93a34
Gourav <junejagourav2006@gmail.com>
2026-04-26 11:23:31 +0530

    feat: integrate Google GenAI for interview report generation and add new report model
    - Added @google/genai dependency for AI functionalities.
    - Implemented generateInterviewReport function in ai.service.js to create structured interview reports based on candidate data.
    - Created report.model.js to define the schema for storing interview reports, including technical and behavioral questions, skill gaps, and preparation plans.
    - Updated server.js to generate an interview report on startup using sample data.
    - Introduced puppeteer for PDF generation from HTML content in the resume generation process.

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
-require('dotenv').config();
-const app = require('./src/app');
+require("dotenv").config();
+const app = require("./src/app");
+const connectDB = require("./src/config/db");
 
-const connectDB = require('./src/config/db');
-
+const { generateInterviewReport } = require("./src/services/ai.service");
+const { resume, selfDescription, JD } = require("./src/services/temp");
 
 connectDB();
+generateInterviewReport({resume, JD, selfDescription});
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running`);
