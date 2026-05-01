2ecc8958e28ca0328e310fc8b20bdc562ab9b4b0
Gourav <junejagourav2006@gmail.com>
2026-05-01 15:05:56 +0530

    Enhance cookie security for login and logout functions

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -75,7 +75,12 @@ async function loginUser(req, res) {
       },
     );
 
-    res.cookie("token", token);
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
     res.status(200).json({
       message: "User logged in successfully",
       user: { id: user._id, username: user.username, email: user.email },
@@ -97,7 +102,11 @@ async function logoutUser(req, res) {
     const token = req.cookies.token;
     if (token) {
       await blacklistTokenModel.create({ token });
-      res.clearCookie("token");
+      res.clearCookie("token", {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+      });
       res.status(200).json({ message: "User logged out successfully" });
     }
   } catch (error) {
