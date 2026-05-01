412043e8efd6acf70071c49d551a0c6dea5afcc5
Gourav <junejagourav2006@gmail.com>
2026-05-01 14:50:14 +0530

    update CORS

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://role-prompt.vercel.app/"],
+    origin: ["http://localhost:5173", "https://role-prompt.vercel.app"],
     credentials: true,
   }),
 );
