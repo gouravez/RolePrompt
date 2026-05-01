453d291042b653aa0e4f15053fc09dcb5ecdd4f9
Gourav <junejagourav2006@gmail.com>
2026-05-01 14:49:42 +0530

    update CORS origin to allow multiple domains

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://role-prompt.vercel.app/"],
     credentials: true,
   }),
 );
